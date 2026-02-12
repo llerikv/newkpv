736a1703eab8fd68023c64126942d479843d429f
llerikv <llerikv@icloud.com>
2026-02-12 13:06:18 +0900

    ee

--- src/components/CounterButton.tsx
+++ src/components/CounterButton.tsx
@@ -5,7 +5,7 @@ export default function CounterButton ({title,initialValue,step}) {
     const [count,setCount] = useState(initialValue);
     return (
       <View style={{ flex: 1, alignItems: "center", justifyContent: "center"}}>
-      <Text>(count)</Text>
+      <Text>{count}</Text>
       <Button title={title} onPress={()  => setCount (count + step)}/>
       </View>
     );
