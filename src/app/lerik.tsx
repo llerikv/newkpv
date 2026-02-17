2cf45c7738ebb0568784db0131489c509c29af6c
llerikv <llerikv@icloud.com>
2026-02-17 17:46:17 +0900

    e

--- src/app/lerik.tsx
+++ src/app/lerik.tsx
@@ -3,7 +3,7 @@ import { Text, View } from "react-native";
 export default function Lerik() {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Albina works</Text>
+      <Text>Lerik's works</Text>
     </View>
   );
 }
