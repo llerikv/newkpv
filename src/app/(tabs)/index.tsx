925b00408837a9ed1e55c3aae856a2838b643173
llerikv <llerikv@icloud.com>
2026-03-24 01:39:31 +0900

    sdelala pochti

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,35 +1,34 @@
-import CounterButton from "@/components/CounterButton";
+// import TodoScreen from "@/screens/Todoscreen";
+// import { View } from "react-native";
+
+// export default function HomeScreen() {
+//   return (
+//     /*<View style={{ flex: 1, backgroundColor: "#fff" }}>
+//       <CounterButton title="Sign in " step={1} initialValue={0}/>
+//       <ProfileCard name="Albinaaa" role="studentka " course="4"></ProfileCard>
+//     </View>
+//   );
+// }*/
+//     <View style={{ flex: 1, backgroundColor: "#fff" }}>
+//       <TodoScreen></TodoScreen>
+//       {/* <ProfileCard name="asd" course="DEV" role="Student" btnName="Press me" btnStep={1056} */}
+
+//       {/* <CounterButton title="Click Me1" step={1} initialValue={0} />
+//       <CounterButton title="Click Me2" step={2} initialValue={0} />
+//       <CounterButton title="Click Me3" step={3} initialValue={0} />
+//       <CounterButton title="Click Me4" step={4} initialValue={0} /> */}
+
+//       {/* <Greeting name="asd" role="teacher"/> */}
+//     </View>
+//   );
+// }
+import TodoScreen from "@/screens/TodoScreen";
 import { View } from "react-native";
 
 export default function HomeScreen() {
-  return(
-    <View style={{flex: 1, backgroundColor: "#fff" }}>
-      <CounterButton title="Click Me1" step={1} initialValue={0}/>
-      <CounterButton title="Click Me2" step={2} initialValue={0}/>
-      <CounterButton title="Click Me3" step={3} initialValue={0}/>
-      <CounterButton title="Click Me4" step={4} initialValue={0}/>
+  return (
+    <View style={{ flex: 1, backgroundColor: "#fff" }}>
+      <TodoScreen/>
     </View>
   );
 }
-
-
-
-/*import ParallaxScrollView from "@/components/parallax-scroll-view";
-import { ProfileCards } from "@/components/ProfileCards";
-import { Image } from "expo-image";
-
-export default function HomeScreen() {
-  return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: "#A1CEDC", dark: "#1D3D47" }}
-      headerImage={
-        <Image
-          source={require("../../assets/images/lake.jpg")}
-          style={{ width: "100%", height: "100%" }}
-        />
-      }
-    >
-      <ProfileCards />
-    </ParallaxScrollView>
-  );
-}*/
--- src/app/(tabs)/lerik.tsx
+++ src/app/(tabs)/lerik.tsx
@@ -1,9 +1,29 @@
-import { Text, View } from "react-native";
+import { Link } from "expo-router";
+import { StyleSheet } from "react-native";
 
-export default function lerik() {
+import { ThemedText } from "@/components/themed-text";
+import { ThemedView } from "@/components/themed-view";
+
+export default function AlbinaScreen() {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>lera works</Text>
-    </View>
+    <ThemedView style={styles.container}>
+      <ThemedText type="title">This is a modal</ThemedText>
+      <Link href="/" dismissTo style={styles.link}>
+        <ThemedText type="link">Albina</ThemedText>
+      </Link>
+    </ThemedView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 20,
+  },
+  link: {
+    marginTop: 15,
+    paddingVertical: 15,
+  },
+});
